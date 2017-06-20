2a45409c7b8afa2fdda61b298dfd596efe9e7a63
cryptic20 <sherwin.wyco20@gmail.com>
2017-06-19 20:52:29 -0400

    modifying json package

--- app.js
+++ app.js
@@ -1,22 +1,9 @@
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
+
+
 const app = express();
-
-
-
-const http = require('http');
-
-const port = parseInt(process.env.PORT, 10) || 8000;
-app.set('port', port);
-
-const server = http.createServer(app);
-
-server.listen(port, () => {
-  console.log(`The server is running at localhost: 8000`);
-});
-
-
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -25,5 +12,6 @@ require('./server/routes')(app);
 app.get('*', (req, res) => res.status(200).send({
   message: 'Welcome to the beginning of nothingness.',
 }));
+
 
 module.exports = app;
