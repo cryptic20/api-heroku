f0e6be304f8813d0f7cf86f874057cbff758fbab
cryptic20 <sherwin.wyco20@gmail.com>
2017-06-19 20:37:17 -0400

    remodified app.js and package.json

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
-
 const app = express();
+
+
+
+const http = require('http');
+
+const port = parseInt(process.env.PORT, 10) || 8000;
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port, () => {
+  console.log(`The server is running at localhost: 8000`);
+});
+
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
