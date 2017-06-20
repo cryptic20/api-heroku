2bcb94c9d2b775bc6aeb7039ca0a4a2bd8ba3066
cryptic20 <sherwin.wyco20@gmail.com>
2017-06-20 14:02:11 -0400

    new app heroku

--- server/models/index.js
+++ server/models/index.js
@@ -6,7 +6,14 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(`${__dirname}/../config/config.json`)[env];
 const db = {};
 
-var sequelize;if (process.env.DATABASE_URL) {      sequelize = new Sequelize(process.env.DATABASE_URL);    } else {      sequelize = new Sequelize(config.database, config.username, config.password, config);    }
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable]);
+} else {
+  sequelize = new Sequelize(
+    config.database, config.username, config.password, config
+  );
+}
 
 fs
   .readdirSync(__dirname)
