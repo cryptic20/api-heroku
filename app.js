9182b289c06ea40b5185b3ee570cd14c8c3c02da
cryptic20 <sherwin.wyco20@gmail.com>
2017-06-20 10:39:13 -0400

    added some stuff

--- app.js
+++ app.js
@@ -2,34 +2,15 @@ const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 
-
-const http = require('http');
-// const app = require('app'); // The express app we just created
-
-
 const app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-
-// const port = parseInt(process.env.PORT, 10) || 8000;
-// app.set('port', port);
-
-// const server = http.createServer(app);
-// server.listen(port, () => {
-//   console.log(`The server is running at localhost:${port}`);
-// });
-
-
+// Require our routes into the application.
 require('./server/routes')(app);
 app.get('*', (req, res) => res.status(200).send({
   message: 'Welcome to the beginning of nothingness.',
 }));
-
-
-
-
-
 
 module.exports = app;
